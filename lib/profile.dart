c95c8c0550aa51cb677ea1c46007ced0152faad6
NidhinAjay <nidhinajay1253@gmail.com>
2023-01-16 12:03:24 +0530

    commit 2

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class bio extends StatefulWidget {
   const bio({Key? key}) : super(key: key);
@@ -10,30 +11,59 @@ class bio extends StatefulWidget {
 class _bioState extends State<bio> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Container(
-            width: double.maxFinite,
-            height: double.maxFinite,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60),
-                    child: Text("Nidhin Ajay P",style:TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-                  ),
-                  SizedBox(height: 50,),
-                  Text("well in this world.",
-                    style:TextStyle(fontWeight: FontWeight.w500,fontSize: 24,),),
-                ],
+    return Scaffold(
+      appBar: AppBar(actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: InkWell(onTap: () {
+            FirebaseAuth.instance.signOut();
+          },
+              child: Icon(
+            Icons.logout,
+            color: Colors.black,
+          )),
+        )
+      ], backgroundColor: Colors.white),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: 75,
+                backgroundImage: AssetImage(
+                  'images/nidhin.jpeg',
+                ),
               ),
-            ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Nidhin Ajay.P",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                    "Innovative and enthusiastic computer science graduate with a passion for critical thinking , problem solving and sharing a love for technology and science. Seeking a position at the digital world, where  this Industrious"
+                    " recent computer science graduate with a zeal for innovation can perform well in this world.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.black87),
+                    textAlign: TextAlign.justify),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
